367b33542ce095af447a72be83d204238e7805ce
VanMo3923 <mjnhhuyen0@gmail.com>
2024-11-28 08:12:20 +0700

    version old

---
+++ StudentManage.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace StudentManage
+{
+    partial class StudentManage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbFullName = new System.Windows.Forms.TextBox();
+            this.tbDateOfBirth = new System.Windows.Forms.TextBox();
+            this.tbClass = new System.Windows.Forms.TextBox();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lbMaSinhVien = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý sinh viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "mã sinh viên :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "họ và tên :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(316, 57);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ngày sinh :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(316, 106);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "lớp :";
+            // 
+            // tbFullName
+            // 
+            this.tbFullName.Location = new System.Drawing.Point(122, 106);
+            this.tbFullName.Margin = new System.Windows.Forms.Padding(2);
+            this.tbFullName.Name = "tbFullName";
+            this.tbFullName.Size = new System.Drawing.Size(122, 20);
+            this.tbFullName.TabIndex = 6;
+            // 
+            // tbDateOfBirth
+            // 
+            this.tbDateOfBirth.Location = new System.Drawing.Point(405, 57);
+            this.tbDateOfBirth.Margin = new System.Windows.Forms.Padding(2);
+            this.tbDateOfBirth.Name = "tbDateOfBirth";
+            this.tbDateOfBirth.Size = new System.Drawing.Size(122, 20);
+            this.tbDateOfBirth.TabIndex = 7;
+            // 
+            // tbClass
+            // 
+            this.tbClass.Location = new System.Drawing.Point(405, 106);
+            this.tbClass.Margin = new System.Windows.Forms.Padding(2);
+            this.tbClass.Name = "tbClass";
+            this.tbClass.Size = new System.Drawing.Size(122, 20);
+            this.tbClass.TabIndex = 8;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Checked = true;
+            this.rbMale.Location = new System.Drawing.Point(4, 27);
+            this.rbMale.Margin = new System.Windows.Forms.Padding(2);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(59, 25);
+            this.rbMale.TabIndex = 10;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "nam";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbFemale);
+            this.groupBox1.Controls.Add(this.rbMale);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(9, 149);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(216, 55);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "giới tính";
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(77, 27);
+            this.rbFemale.Margin = new System.Windows.Forms.Padding(2);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(47, 25);
+            this.rbFemale.TabIndex = 11;
+            this.rbFemale.Text = "nữ";
+            this.rbFemale.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(9, 228);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(581, 175);
+            this.dataGridView.TabIndex = 12;
+            this.dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(320, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(486, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(405, 12);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "Cập nhật";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.MouseClick += new System.Windows.Forms.MouseEventHandler(this.clickUpdate);
+            // 
+            // lbMaSinhVien
+            // 
+            this.lbMaSinhVien.AutoSize = true;
+            this.lbMaSinhVien.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMaSinhVien.Location = new System.Drawing.Point(128, 57);
+            this.lbMaSinhVien.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbMaSinhVien.Name = "lbMaSinhVien";
+            this.lbMaSinhVien.Size = new System.Drawing.Size(19, 21);
+            this.lbMaSinhVien.TabIndex = 16;
+            this.lbMaSinhVien.Text = "1";
+            // 
+            // StudentManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 413);
+            this.Controls.Add(this.lbMaSinhVien);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbClass);
+            this.Controls.Add(this.tbDateOfBirth);
+            this.Controls.Add(this.tbFullName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "StudentManage";
+            this.Text = "StudentManage";
+            this.Load += new System.EventHandler(this.StudentManage_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbFullName;
+        private System.Windows.Forms.TextBox tbDateOfBirth;
+        private System.Windows.Forms.TextBox tbClass;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lbMaSinhVien;
+    }
+}
---
+++ StudentManage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManage
+{
+    public partial class StudentManage : Form
+    {
+        QuanLySinhVienEntities database = new QuanLySinhVienEntities();
+        List<SinhVien> danhSachSinhVien;
+
+        DataTable dataTable;
+
+        public StudentManage()
+        {
+           
+            InitializeComponent();
+            initDataTable();
+          
+        }
+        private void initDataTable()
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("MaSinhVien", typeof(int));
+            dataTable.Columns.Add("HoTen", typeof(string));
+            dataTable.Columns.Add("NgaySinh", typeof(DateTime));
+            dataTable.Columns.Add("Lop", typeof(string));
+            dataTable.Columns.Add("GioiTinh", typeof(string));
+        }
+        private void StudentManage_Load(object sender, EventArgs e)
+        {
+            danhSachSinhVien = database.SinhViens.ToList();
+            foreach (var item in danhSachSinhVien)
+            {
+                dataTable.Rows.Add(item.MaSinhVien, item.HoTen, item.NgaySinh, item.Lop,item.GioiTinh);
+            }
+
+            dataGridView.DataSource = dataTable;
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(lbMaSinhVien.Text);
+            string fullname = tbFullName.Text;
+            string dob = tbDateOfBirth.Text;
+            string lop = tbClass.Text;
+            string sex = rbMale.Checked ? "nam" : "nữ";
+
+            SinhVien sinhVien = new SinhVien(id, fullname, DateTime.Parse(dob), lop, sex);
+
+            dataTable.Rows.Add(id, fullname, dob, lop, sex);
+
+            database.SinhViens.Add(sinhVien);
+            database.SaveChanges();
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView.SelectedRows[0];
+                string id = selectedRow.Cells["MaSinhVien"].Value.ToString();
+                string fullname = selectedRow.Cells["HoTen"].Value.ToString();
+                string dob = selectedRow.Cells["NgaySinh"].Value.ToString().Split(' ')[0];
+                string lop = selectedRow.Cells["Lop"].Value.ToString();
+                string sex = selectedRow.Cells["GioiTinh"].Value.ToString();
+                lbMaSinhVien.Text = id;
+                tbFullName.Text = fullname;
+                tbDateOfBirth.Text = dob;
+                tbClass.Text = lop;
+                if (sex == "nam")
+                {
+                    rbMale.Checked = true;
+                }
+                else
+                {
+                    rbFemale.Checked = true;
+
+                }
+            }
+        }
+
+        private void clickUpdate(object sender, MouseEventArgs e)
+        {
+            int id = int.Parse(lbMaSinhVien.Text);
+            string fullname = tbFullName.Text;
+            string dob = tbDateOfBirth.Text;
+            string lop = tbClass.Text;
+            string sex = rbMale.Checked ? "nam" : "nữ";
+
+            var sinhVien = database.SinhViens.FirstOrDefault(sv => sv.MaSinhVien == id);
+
+            if (sinhVien != null)
+            {
+                // Cập nhật thông tin
+                sinhVien.HoTen = fullname;
+                sinhVien.NgaySinh = DateTime.Parse(dob);
+                sinhVien.Lop = lop;
+                sinhVien.GioiTinh = sex;
+
+                // Lưu thay đổi
+                database.SaveChanges();
+
+                MessageBox.Show($"Cập nhật thành công sinh viên có mã : {id}");
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy sinh viên với ID được chỉ định.");
+            }
+        }
+    }
+}
